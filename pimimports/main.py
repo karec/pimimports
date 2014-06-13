d597608459c051c08657bee3980c683965ecdd58
evalette <evalette@expertime.com>
2014-06-13 11:59:57 +0200

    done but need parameters for execute pim imports

--- pimimports/main.py
+++ pimimports/main.py
@@ -1,20 +1,10 @@
 import yaml
+from tasks import play_all
 
 
 stream = file('imports.yaml', 'r')
 imports = yaml.load(stream)
 
 
-def play_import(name, value, imports):
-    after = value.get('after', None)
-    if after is not None  and after in imports:
-        play_import(after, imports.get(after), imports)
-    print "playing %s..." % (name)
-    if name in imports:
-        imports.pop(name)
-    return imports
-
 if __name__ == '__main__':
-    while imports:
-        key, value = imports.popitem()
-        imports = play_import(key, value, imports)
+    play_all.delay(imports)
--- pimimports/tasks.py
+++ pimimports/tasks.py
@@ -6,16 +6,18 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 @app.task
-def play_all():
-    stream = file('imports.yaml', 'r')
-    imports = yaml.load(stream)
+def play_all(imports):
+    logger.info('start import')
     while imports:
         key, value = imports.popitem()
         imports = play_import(key, value, imports)
     return True
 
 def play_import(key, value, imports):
-    play_one.apply_async(('produit',), link=[play_one.si('cat'), play_one.si('pneu')])
+    if value.get('after', None):
+        play_one.apply_async((key,), link=[play_one.si(name) for name in value.get('after')])
+    else:
+        play_one.delay(key)
     return imports
 
 @app.task
