98e1a50d7135a91dd0d14235565e9509a025587d
evalette <evalette@expertime.com>
2014-06-27 19:19:32 +0200

    Refactor some code, adding comments and clean some code for pep8

--- pimimports/settings.py
+++ pimimports/settings.py
@@ -1,2 +1,6 @@
-PIM_ABS_PATH = '/home/evalette/Projets/FeuVert-PIM'
+# The absolute path of your PIM installation folder
+PIM_ABS_PATH = '/your/path/to/pim/folder'
+# The path of the php bin
 PHP_PATH = 'php'
+# The needed environment for your exports
+ENV = 'prod'
--- pimimports/tasks.py
+++ pimimports/tasks.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import yaml
 from pimimports.celery import app
 from celery.utils.log import get_task_logger
 from subprocess import call
@@ -9,16 +8,33 @@ from celery import chain
 
 logger = get_task_logger(__name__)
 
-def play_all(imports):
-    logger.info('start import')
-    while imports:
-        key, value = imports.popitem()
-        imports = play_import(key, value, imports)
-    return True
 
-def play_import(key, value, imports):
+def play_all(jobs):
+    """
+    This function will add all jobs in the jobs dict to queue
+    It will pop all items of the dict
+    :param jobs: dict of all jobs to play
+    :return: the jobs dict, if all the process is good, it must be empty
+    """
+    logger.info('start jobs')
+    while jobs:
+        key, value = jobs.popitem()
+        jobs = play_import(key, value, jobs)
+    return jobs
+
+
+def play_import(key, value, jobs):
+    """
+    This function will play an import.
+    If the import has an after key, a chain will be generate and play.
+    In other case, it just use the task play_one the start the import, passing the key parameter
+    :param key: string key of the job in dict
+    :param value: string the value of this key
+    :param jobs: dict the whole dictionary
+    :return: dict the dictionary jobs
+    """
     if value.get('after', None):
-        subtasks = []
+        subtasks = list()
         subtasks.append(play_one.si(key))
         for name in value.get('after'):
             subtasks.append(play_one.si(name))
@@ -26,11 +42,18 @@ def play_import(key, value, imports):
         workflow.delay()
     else:
         play_one.delay(key)
-    return imports
+    return jobs
+
 
 @app.task
 def play_one(name):
+    """
+    This function is used for playing async job.
+    It will play one job using user parameters
+    :param name: string name of the job
+    :return: None
+    """
     console_path = os.path.join(settings.PIM_ABS_PATH, 'app/console')
-    exec_path = "%s %s akeneo:batch:job %s" % (settings.PHP_PATH, console_path, name)
+    exec_path = "%s %s akeneo:batch:job %s --env=%s" % (settings.PHP_PATH, console_path, name, settings.ENV)
     logger.info(exec_path)
     call([exec_path], shell=True)
